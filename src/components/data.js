59d4f34fcca65a4d025219bc34c2baabd71f0c7d
Jun Murase <masutadoz@gmail.com>
2024-05-07 23:40:04 +0900

    description

--- src/components/data.js
+++ src/components/data.js
@@ -3,7 +3,7 @@ export const projectData = [
 		id: 1,
 		title: 'tvapp',
 		description:
-			'マルチスレッドBBS(stack:MongoDB+NEXTjs+flask(backend)',
+			'Multi-threaded BBS(stack:MongoDB+NEXTjs+flask(backend)',
 		gitHubLink: 'https://github.com/PlasmaXD/tvapp',
 	},
 	{
